6efdffd2ab599752b60ae25c2e5195d0e6921d83
Irina Ermolenko <you@example.com>
2025-03-03 23:26:56 +0200

    Update index.js to listen on correct port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
-
+// Встановлюємо порт з змінної середовища або 10000 за замовчуванням
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Сайт працює!");
 });
 
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on http://localhost:3000");
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
