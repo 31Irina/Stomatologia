51db3f7aa20c0f916f98c336a03eed90d06dd700
Irina Ermolenko <you@example.com>
2025-03-04 00:35:10 +0200

    Updated index.js to use EJS as templating engine

--- index.js
+++ index.js
@@ -6,14 +6,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on http://localhost:3000");
-});
-app.listen(3000, "0.0.0.0", () => {
     console.log("Server running on http://localhost:3000/services");
-});
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on http://localhost:3000/patients");
-});
-app.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on http://localhost:3000/appointments");
 });
